ca7cf144f26a524ce902a4fe92ad188862618f26
Anton Zlygostev <zlygostev@hotmail.com>
2021-10-11 09:44:44 +0700

    Moved log-related files to a separate folder

---
+++ Mordent.Core/Log/ILog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+#if DEBUG
+#endif
+
+namespace Mordent.Core
+{
+    public interface ILogFile: IDisposable //, IAsyncDisposable
+    {
+        public Lsn Append(ReadOnlySpan<byte> data);
+        public void Flush(Lsn upToLsn);
+        public IEnumerable<byte[]> Records { get; }
+    }
+
+    public interface ILog
+    {
+        public void StartTransaction(DbTranId tranId);
+        public void CommitTransaction(DbTranId tranId);
+        public void RecordChange(DbTranId tranId, object oldData, object newData);
+        public void StartCheckPoint();
+        public void EndCheckPoint();
+
+    }
+}
